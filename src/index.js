e9c73a4d855620c319c760d74331e471952dbb72
Atle Magnussen <atlemagnussen@gmail.com>
2019-05-23 20:35:17 +0200

    try with reference to node_modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import About from "./components/about/about.js";
 import Blog from "./components/blog/blog.js";
-import EzTree from "./lib/eztree.js";
+import EzTree from "../node_modules/es6-tree/public/eztree.js";
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./services/router.js";
