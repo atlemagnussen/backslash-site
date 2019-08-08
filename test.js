2adc321c0016811413ab8da641db45fd9577c5c4
Atle Magnussen <atlemagnussen@gmail.com>
2019-08-08 14:17:38 +0200

    fix jsonfig and eslintæ

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const test = [
     {id: 1, text: "text1"},
     {id: 2, text: "text2"}
 ];
+const fil = test.findIndex(f => f.id === 1);
+
+console.log(fil);
 console.log(test.find((f) => {
     return f.id === 1;
 }).text);
+
